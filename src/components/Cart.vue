491cd5c8ba29220207f0619b136b4bc7bce3f858
musabagja <bagja7musa@gmail.com>
2020-11-19 12:16:59 +0700

    Add Checkout Success

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -107,6 +107,22 @@ export default {
     checkout () {
       this.$store.dispatch('checkout')
         .then(result => {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+
+          Toast.fire({
+            icon: 'success',
+            title: 'Checkout successfully'
+          })
           this.$store.dispatch('getCart')
           this.$store.dispatch('getProducts')
           this.$store.dispatch('getWishlist')
